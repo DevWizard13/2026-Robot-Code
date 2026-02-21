b3c810ca37b9249ab65509409d9cd70c0f7a95ce
DevWizard <spencer.roberts@summitlc.org>
2026-02-21 15:52:13 -0800

    Made New Methods
    No code, I spent most of my time thinking. My brain was kinda stuck
    tday. IDK

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -1,4 +1,4 @@
-/*package frc.robot.subsystems;
+package frc.robot.subsystems;
 import java.util.Optional;
 
 import java.util.*;
@@ -12,6 +12,7 @@ import frc.robot.Constants;
 public class PhotonVision {
 	List<PhotonCamera> robotCameras;
   List<PhotonPoseEstimator> cameraEst = new ArrayList<>();
+
   Pose2d targetPose = new Pose2d();
   DriveSubsystem drive = new DriveSubsystem();
   double turnModifier = 0.01;
@@ -31,8 +32,6 @@ public class PhotonVision {
     	cameraEst.get(i).setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
     }
 	}
-
-  /*
 
 	public Optional<EstimatedRobotPose> getPose(String cameraName) {
 		// check to see which camera matches the desired name, and when it's found,
@@ -69,86 +68,12 @@ public class PhotonVision {
     }
   }
 
-  public boolean aimAtTarget() {
-    // I made it a bool so you can identify if it's corrrectly aimed
-
-    PhotonCamera camera = robotCameras.get(0);
-    boolean targetVisible = false;
-    double turn = 0.0;
-    double speed = 0.0;
-    List<Double> targetYaw = new ArrayList<>();
-    // I'll replace it with the camera at the front
-
-    for (PhotonCamera myCamera : robotCameras) {
-      var results = myCamera.getAllUnreadResults();
-      
-      if (!results.isEmpty()) {
-        // Camera processed a new frame since last
-        // Get the last one in the list.
-      
-        var result = results.get(results.size() - 1);
-      
-        if (result.hasTargets()) {
-          // At least one AprilTag was seen by the camera
-          for (var target : result.getTargets()) {
-            if (target.getFiducialId() == 7) {
-              // Found Tag 7, record its information
-              targetYaw.add(target.getYaw() + 
-                  offset[robotCameras.indexOf(myCamera)]);
-      
-              targetVisible = true;
-            }
-          }
-        }
-      }
-    }
-
-    double avgTargetYaw = calculateAvg(targetYaw);
-
-    if (targetVisible) {
-      if (Math.abs(avgTargetYaw) > 5.0) {
-        if (avgTargetYaw > 0) {
-          turn = -1.0;
-        } else {
-          turn = 1.0;
-        }
-      }
-      else if (Math.abs(avgTargetYaw) < 5.0) {
-        if (avgTargetYaw > 0) {
-          turn = -0.5;
-        } else {
-          turn = 0.5;
-        }
-      } 
-    }
-    else {
-      System.out.println("Target not visible");
-    }
-
-    if (getDistanceToTag() > maxDistance) {
-      speed = 1.0;
-      drive.arcadeDrive(speed, turn);
-      return false;
-    }
-
-    drive.arcadeDrive(speed, turn);
-
-    if (Math.round(avgTargetYaw) != 0) {
-      return true;
-    }
-    else {
-      return false;
-    }
+  public double getRotToTarget() {
+    Pose2d pos = getPose(Constants.vision.localizationCameraName[0]).get().estimatedPose.toPose2d();
   }
 
-  double calculateAvg(List<Double> val) {
-    double x = 0.0;
-    for (double i : val){
-      x += i;
-    };
-    x = x / val.size();
-    return x;
+  public double getDriveToTarget() {
+    return 0.0;
   }
-  */
 }
-*/
+
